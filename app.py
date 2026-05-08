352d4ffc57295d971996c21e3826f0d011772e02
Sudheer Kumar <sudheermsdvk@gmail.com>
2026-05-08 17:27:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ app = Flask(__name__)
 
 app.config['MAIL_SERVER'] = 'smtp.outlook.com'
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = 'sudheerdata@outlook.com'
-app.config['MAIL_PASSWORD'] = 'Sudheer@123'
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 
